2e0f16e7f79f5cd48aef52d28cb4eddf863e0b40
Vashishth Pathak <pathakvashishth05@gmail.com>
2026-07-03 14:15:35 +0530

    Deployment on vercel

---
+++ api/index.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { AppModule } from '../src/app.module';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }),
+  );
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Event Booking System API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.init();
+}
+
+bootstrap();
+
+export default server;
