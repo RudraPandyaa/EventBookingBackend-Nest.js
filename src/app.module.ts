1d5caa392ef5322217cce42bd2706be5a1938648
Vashishth Pathak <pathakvashishth05@gmail.com>
2026-07-03 15:26:46 +0530

    removed mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { BookingsModule } from './bookings/bookings.module';
 import { PrismaService } from './prisma/prisma.service';
@@ -15,7 +14,6 @@ import { PaymentModule } from './payment/payment.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
     EventsModule,
     BookingsModule,
     PrismaModule,
